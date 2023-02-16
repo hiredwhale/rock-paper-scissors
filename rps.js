8175b1c70e7ad24a710f33f750b470d26bf30b29
Jonathan Frederick <jon.ed.fred@gmail.com>
2023-02-15 21:23:58 -0600

    Reworked js to use buttons for player selection

--- rps.js
+++ rps.js
@@ -1,56 +1,32 @@
-const choiceGroups = [ ["Rock", "Paper", "Scissors"], ["Paper", "Scissors", "Rock"], ["Scissors", "Rock", "Paper"] ];
+const choiceGroups = [ ["Scissors", "Rock", "Paper"], ["Rock", "Paper", "Scissors"], ["Paper", "Scissors", "Rock"] ];
 var choices = [];
 
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        playRound(button.id);
+    });
+});
+
 function getComputerChoice() {
-    choices = choiceGroups[Math.floor(Math.random() * 3)];
-    return choices[1];
+    return Math.floor(Math.random() * 3);
 }
 
-function getPlayerChoice() {
-    return prompt("Rock, Paper, or Scissors?");
+function setPlayerChoice(choice) {
+    choices = choiceGroups[Number(choice) - 1];
+    return 1;
 }
 
-function playRound(playerSelection, computerSelection) {
-    let computerIndex = 1;
-    let playerIndex = choices.indexOf(
-        playerSelection.charAt(0).toUpperCase()+
-        playerSelection.substring(1).toLowerCase()
-    );
+function playRound(playerSelection) {
+    let playerIndex = setPlayerChoice(playerSelection);
+    let computerIndex = getComputerChoice();
 
-    if (playerIndex < 0) {
-        console.log(`${playerSelection} is not a valid option! `+
-            `Please try again.`);
-
-        return playRound(getPlayerChoice(), getComputerChoice());
-    } else if (playerIndex < computerIndex) {
-        console.log(`You Lose! ${computerSelection} beats ${choices[playerIndex]}`);
-        return - 1;
+    if (playerIndex < computerIndex) {
+        console.log(`You Lose! ${choices[computerIndex]} beats ${choices[playerIndex]}`);
     } else if (playerIndex > computerIndex) {
-        console.log(`You Win! ${choices[playerIndex]} beats ${computerSelection}`);
-        return 1;
+        console.log(`You Win! ${choices[playerIndex]} beats ${choices[computerIndex]}`);
     } else {
-        console.log(`Draw! You both picked ${computerSelection} `+
+        console.log(`Draw! You both picked ${choices[playerIndex]}. `+
             `Please try again.`);
-
-        return playRound(getPlayerChoice(), getComputerChoice());
     }
 }
-
-function game() {
-    let playerWins = 0;
-    let computerWins = 0;
-
-    for (let i = 0; i < 5; i++) {
-        let result = playRound(getPlayerChoice(), getComputerChoice());
-
-        if (result === -1) {
-            computerWins++;
-        } else {
-            playerWins++;
-        }
-    }
-
-    console.log(`Your record was ${playerWins}-${computerWins}`)
-}
-
-game();
